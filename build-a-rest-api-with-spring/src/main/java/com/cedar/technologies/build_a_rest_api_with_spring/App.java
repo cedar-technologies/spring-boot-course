095e31f6d5efb363bf8707c18d5a3cd05abd5fcd
alex <alexandre.jx@gmail.com>
2019-06-06 09:26:02 -0700

    Adding new app

---
+++ build-a-rest-api-with-spring/src/main/java/com/cedar/technologies/build_a_rest_api_with_spring/App.java
@@ -0,0 +1,13 @@
+package com.cedar.technologies.build_a_rest_api_with_spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- spring-boot-api/bootstrap-web-application/src/main/java/com/cedar/technologies/bootstrap_web_application/SpringBootRestApplication.java
+++ spring-boot-api/bootstrap-web-application/src/main/java/com/cedar/technologies/bootstrap_web_application/SpringBootRestApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootRestApplication {
 
-	
 	public static void main(String[] args) {
 		
+
 		SpringApplication.run(SpringBootRestApplication.class, args);
 		
 	}
